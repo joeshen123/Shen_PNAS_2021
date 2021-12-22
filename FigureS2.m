68e9cad6d26641fe286372c63e6ad6f99a6ee0e1
Zhouyang Shen <shenz@mskcc.org>
2021-12-21 19:27:34 -0500

    Minor Changes

--- FigureS2.m
+++ FigureS2.m
@@ -1,29 +1,14 @@
-%FigureS2b: 
-%Plot GUV radius distribution
 
-figure(34), histogram(GUVdistribution.radius_micron);
+%Figure S1d cPla2 C2-eGFP Calcium Mutant binding (60 min)
+WT=D3487_WT.NormalizedGFPintensity(find(D3487_WT.TimePoint==60));
+K=D3487K.NormalizedGFPintensity(find(D3487K.TimePoint==60));
+R=D3487R.NormalizedGFPintensity(find(D3487R.TimePoint==60));
 
-%Figure S2c: Check GUV unilamellarity and circularity
+Mat_D3487=padcat(WT,K,R);
+figure(36), dotplot(Mat_D3487,0.1, 0.01);
+ylim([0,70]);
 
-%plot GUV unilamellar percentage original vs. modified
-
-GUVuni_old=GUVFractionUniLam.UnilamellarRatio(find(GUVFractionUniLam.Approaches=='Original Approach'));
-GUVuni_new=GUVFractionUniLam.UnilamellarRatio(find(GUVFractionUniLam.Approaches=='Modified Approach'));
-
-Mat_GUVuni=padcat(GUVuni_old, GUVuni_new);
-figure(35), dotplot(Mat_GUVuni,0.1,0.01);
-
-
-%Unilamellar Percentage Statistical Test
-[h,p] = ttest2(GUVuni_old,GUVuni_new);
-
-%plot GUV circularity original vs. modified
-
-GUVcirc_old=GUVCircularity.Circularity(find(GUVCircularity.Approaches=='Original Approach'));
-GUVcirc_new=GUVCircularity.Circularity(find(GUVCircularity.Approaches=='Modified Approach'));
-
-Mat_GUVcirc=padcat(GUVcirc_old, GUVcirc_new);
-figure(36), dotplot(Mat_GUVcirc,0.1,0.01);
-
-%Circularity Statistical Test
-[h1,p1] = ttest2(GUVcirc_old,GUVcirc_new);
+%WT and D3487K Statistical Test
+[h,p] = ttest2(WT,K);
+%WT and D3487R Statistical Test
+[h1,p1] = ttest2(WT,R);
